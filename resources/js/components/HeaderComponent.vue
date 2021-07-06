cdeb340c43aac93f19555b0f57bf95d6e1033db6
yuuki_takahara <takahara.yuuki22@gmail.com>
2021-07-06 21:18:21 +0900

    add: ベースのbladeとルーティングの追加

---
+++ resources/js/components/HeaderComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container-fluid bg-dark mb-3">
+    <div class="container">
+      <div class="navbar navbar-dark">
+        <span class="navbar-brand mb-0 h1">Vue Laravel SPA</span>
+        <div>
+          <button class="btn btn-success">List</button>
+          <button class="btn btn-success">ADD</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
